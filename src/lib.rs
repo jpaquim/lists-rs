42df1fc354916d320bef12744f5b6a4283ddc8fc
jpaquim <joaopaquim@gmail.com>
2020-02-12 00:04:41 +0000

    Add silly1.rs with double singly-linked list
    Reuse singly-linked Stack from first examples

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ pub mod fifth;
 pub mod first;
 pub mod fourth;
 pub mod second;
+pub mod silly1;
 pub mod third;
 pub mod third_arc;
---
+++ src/silly1.rs
@@ -0,0 +1,64 @@
+pub struct List<T> {
+    left: Stack<T>,
+    right: Stack<T>,
+}
+
+impl<T> List<T> {
+    fn new() -> Self {
+        List {
+            left: Stack::new(),
+            right: Stack::new(),
+        }
+    }
+}
+
+pub struct Stack<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { head: None }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            let node = *node;
+            self.head = node.next;
+            node.elem
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+}
+
+impl<T> Drop for Stack<T> {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
+    }
+}
