518607ab47f1214b5f835a55235e8c2742a768a8
jpaquim <joaopaquim@gmail.com>
2020-02-12 00:10:34 +0000

    Implement _left and _right versions

--- src/silly1.rs
+++ src/silly1.rs
@@ -10,6 +10,57 @@ impl<T> List<T> {
             right: Stack::new(),
         }
     }
+
+    pub fn push_left(&mut self, elem: T) {
+        self.left.push(elem)
+    }
+
+    pub fn push_right(&mut self, elem: T) {
+        self.right.push(elem)
+    }
+
+    pub fn pop_left(&mut self) -> Option<T> {
+        self.left.pop()
+    }
+
+    pub fn pop_right(&mut self) -> Option<T> {
+        self.right.pop()
+    }
+
+    pub fn peek_left(&self) -> Option<&T> {
+        self.left.peek()
+    }
+
+    pub fn peek_right(&self) -> Option<&T> {
+        self.right.peek()
+    }
+
+    pub fn peek_left_mut(&mut self) -> Option<&mut T> {
+        self.left.peek_mut()
+    }
+
+    pub fn peek_right_mut(&mut self) -> Option<&mut T> {
+        self.right.peek_mut()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn walk_aboot() {
+        let mut list = List::new();
+
+        list.push_left(0);
+        list.push_right(1);
+        assert_eq!(list.peek_left(), Some(&0));
+        assert_eq!(list.peek_right(), Some(&1));
+
+        list.push_left(2);
+        list.push_left(3);
+        list.push_right(4);
+    }
 }
 
 pub struct Stack<T> {
