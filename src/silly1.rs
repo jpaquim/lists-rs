ab7ed17de3a56ea82c10720149482bee2e73fdc7
jpaquim <joaopaquim@gmail.com>
2020-02-12 00:13:51 +0000

    Refactor Stack push and pop using helper _node functions

--- src/silly1.rs
+++ src/silly1.rs
@@ -82,17 +82,25 @@ impl<T> Stack<T> {
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: self.head.take(),
+            next: None,
         });
 
-        self.head = Some(new_node);
+        self.push_node(new_node);
+    }
+
+    fn push_node(&mut self, mut node: Box<Node<T>>) {
+        node.next = self.head.take();
+        self.head = Some(node);
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|node| {
-            let node = *node;
-            self.head = node.next;
-            node.elem
+        self.pop_node().map(|node| node.elem)
+    }
+
+    fn pop_node(&mut self) -> Option<Box<Node<T>>> {
+        self.head.take().map(|mut node| {
+            self.head = node.next.take();
+            node
         })
     }
 
